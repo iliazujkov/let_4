36f840f32858fa8cc2660541dc92a1f0dc59953a
iliazujkov <90790199+iliazujkov@users.noreply.github.com>
2021-11-08 19:40:18 +0300

    Add files via upload

--- letuchka4/main.cpp
+++ letuchka4/main.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 #include <iomanip>
 struct MobilePhone
-        {
+{
     double weight;
     bool is_broken;
     double price;
-        };
-
-/// лишние файлы в репозитории
-/// неправильное оформление
+    void toXML(MobilePhone* Mp, int n) {
+        std::cout << std::boolalpha;
+        std::cout << std::fixed;
+        std::cout << "<MobilePhones>\n";
+        for (int i = 0; i < n; ++i) {
+            std::cout << "\t<MobilePhone id=" << "\"" << i << "\"" << std::setprecision(1) << " weight=" << "\"" << Mp[i].weight << "\"" << "  is broken=" << "\"" << Mp[i].is_broken << "\"" << "  price=";
+            (int(Mp[i].price * 10) % 10 == 0) ? std::cout << std::setprecision(0) << "\"" << Mp[i].price << "\"/> \n" : std::cout << std::setprecision(2) << "\"" << Mp[i].price << "\"/> \n";
+        }
+        std::cout << "</MobilePhones>\n";
+    }
+};
 int main() {
     int n;
     std::cin >> n;
@@ -18,12 +25,6 @@ int main() {
         std::cin >> Mp[i].is_broken;
         std::cin >> Mp[i].price;
     }
-    std::cout << std::boolalpha;
-    std::cout << std::fixed << std::setprecision(1);
-    std::cout << "<MobilePhones>\n";
-    for (int i = 0; i < n; ++i) {
-        std::cout << "\t<MobilePhone id=\"" << i << "\" weight=\"" << Mp[i].weight << "\"  is broken=\"" << Mp[i].is_broken << "\"  price=\"" << Mp[i].price << "\"/>\n";
-    }
-    std::cout << "</MobilePhones>\n";
+    Mp->toXML(Mp, n);
     return 0;
 }
